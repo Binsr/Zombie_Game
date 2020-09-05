2fe91d16a00b04523ea220c7c2e973a1afe85be9
Binsr <urosjovanovic.sna@gmail.com>
2020-09-06 00:02:56 +0200

    types in typescript

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-var message = 'First typeScript app';
-console.log('message');
+"use strict";
+exports.__esModule = true;
+var message = 'First typeScript app - Zombie app';
+var br = 10;
+console.log(message + (" " + br));
+var list = [1, 2, 3];
+for (var i = 0; i < list.length; i++) {
+    console.log(list[i]);
+}
+var list2 = [2, 'text'];
+console.log(list2[1]);
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c);
+var myVar = "assert test";
+console.log(myVar.length);
+var Mult;
--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-let message= 'First typeScript app';
+export{}
+let message: string= 'First typeScript app - Zombie app';
+let br: number= 10;
 
-console.log('message');
+
+console.log(message + ` ${br}`);
+
+let list: Array<Number> = [1,2,3];
+
+for(let i= 0; i < list.length; i++){
+    console.log(list[i]);
+}
+
+let list2: [number, string] = [2,'text'];
+
+console.log(list2[1]);
+
+enum Color {Red= 5, Green, Blue};
+
+let c: Color = Color.Green;
+
+console.log(c);
+
+let myVar: unknown = "assert test";
+
+console.log((myVar as string).length);
+
+let Mult: boolean | string;
